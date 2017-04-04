ee30b9933172644c35457b4b3e6cf143e299c74e
lyndon <mostshow@163.com>
2017-04-04 17:20:22 +0800

    Hot-reloading setup

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,15 @@
 
 import React from 'react'
+import Greetings from './Greetings'
 
 
-export default () => {
-    return (
-        <h1>Hello from react</h1>
-    )
+class App extends React.Component {
+    render(){
+        return (
+            <Greetings />
+        )
+
+    }
 }
+
+export default  App;
--- server/index.js
+++ server/index.js
@@ -5,14 +5,26 @@ import path from 'path'
 import webpack from 'webpack'
 import webpackMiddleware from 'webpack-dev-middleware'
 import webpackConfig from '../webpack.config.dev'
+import webpackHotMiddleware from 'webpack-hot-middleware'
 
 let app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)))
+const compiler = webpack(webpackConfig)
+
+app.use(webpackMiddleware(compiler,{
+    hot: true,
+    publicPath: webpackConfig.output.publicPath,
+    noInfo: false
+
+}))
+
+app.use(webpackHotMiddleware(compiler))
+
 
 app.get('/*' ,(req, res) => {
     res.sendfile(path.join(__dirname, './index.html'))
 })
+
 
 app.listen(3000, () => console.log('Running on localhost:3000'))
 
