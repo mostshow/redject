7825c24f4beda0a91e73629c073108d595cdb10d
lyndon <mostshow@163.com>
2017-04-12 12:01:39 +0800

    setting mongodb

--- server/config.js
+++ server/config.js
@@ -3,12 +3,12 @@ export default {
 
     httpPort: 3006,
     localhost: '127.0.0.1', //本地部署
-    devDbUrl: 'mongodb://127.0.0.1/bug',
+    devDbUrl: 'mongodb://127.0.0.1/logReport',
     dbConfig: {
         host: "10.16.15.101",
-        database: "bug",
-        userid: "adeploy_qgz",
-        password: "qgz#2017",
+        database: "logReport",
+        userid: "logReport",
+        password: "qguanzi",
         port:27017,
     },
     debug: true
--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ import webpackMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
 
 import webpackDevConfig from '../webpack.config.dev'
-import webpackConfig from '../webpack.config.babel'
 
 import mongokeeper  from './models/mongokeeper'
 import apiRouter from './routes/apiRouter'
@@ -31,7 +30,6 @@ if(process.env.NODE_ENV == 'development'){
 
 app.use(express.static(path.join(__dirname, '../client')));
 
-process.env.MONGO_DB_STR = config.devDbUrl;//config.dbConfig;
 mongokeeper.config(config.dbConfig);
 
 app.use('/api',apiRouter);
